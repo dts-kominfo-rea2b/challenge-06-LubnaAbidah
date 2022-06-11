633040512bd3f45d6e3f47ccbbd18dc1f5249f5e
Lubna Abidah <lubna16abidah@gmail.com>
2022-06-11 22:17:55 +0700

    update fungsiCustom.js

--- fungsiCustom.js
+++ fungsiCustom.js
@@ -1,5 +1,5 @@
 // TODO: import module bila dibutuhkan di sini
-
+const fs = require('fs');
 // ! JANGAN DIMODIFIKASI
 let file1 = "./data1.json";
 let file2 = "./data2.json";
@@ -18,7 +18,28 @@ let modifyFile3 = (val) => {
 
 // TODO: Kerjakan bacaData
 // gunakan variabel file1, file2, dan file3
-const bacaData = null;
+const bacaData = (fnCallback) =>{
+  let baca = [];
+  fs.readFile(file1, (err, data) => {
+    if (err) throw err;
+    let data1 = JSON.parse(data);
+    return data1.message.split(" ")[1];
+  });
+  
+  fs.readFile(file2, (err, data) => {
+      if (err) throw err;
+      let data2 = JSON.parse(data);
+      return data2[0].message.split(" ")[1];
+    });
+  
+  fs.readFile(file3, (err, data) => {
+      if (err) throw err;
+      let data3 = JSON.parse(data);
+      return data3[0].data.message.split(" ")[1];
+    });
+
+    fnCallback(null, baca);
+};
 
 // ! JANGAN DIMODIFIKASI
 module.exports = {
